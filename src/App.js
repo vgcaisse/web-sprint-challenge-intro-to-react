f14df5fd67e601e93a8901fc14e27c41572645f0
Visnu <vgcaisse@gmail.com>
2021-12-10 15:45:00 -0500

    working on the characters

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import axios from "axios";
+
+import Img from './components/img'
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,8 +13,28 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const baseURL = 'https://swapi.dev/api/people/';
+  const [starWars, setStarWarsData] = useState({})
+ 
+
+  useEffect(() => {
+    const getData = () => {
+      axios.get(baseURL)
+      .then(res => {
+        console.log(res.data)
+        setStarWarsData(res.data)        
+      })
+      .catch(err => {
+        console.error()
+      })
+    }
+    getData();
+  }, []);
+
   return (
     <div className="App">
+      {/* <Character /> */}
+      <Img url={starWars.url} alt={starWars.title}/>
       <h1 className="Header">Characters</h1>
     </div>
   );
